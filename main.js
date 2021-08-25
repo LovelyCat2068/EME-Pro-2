aa6e6619e91d651bf0c1387938fd0568a564ae8d
LovelyCat2068 <76818025+LovelyCat2068@users.noreply.github.com>
2021-08-25 20:39:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+Webcam.set({
+    height: 300,
+    width: 300,
+    image_format: 'jpeg',
+    jpeg_quality: 91
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_snapshot(){
+    Webcam.snap(function(Sc){
+        document.getElementById("result").innerHTML = '<img id="img_hd" src="'+Sc+'"/>';
+    });
+}
+
+console.log("ML5."+ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/af3XsSgUU/model.json',ModelLoaded);
+
+function ModelLoaded(){
+    console.log("Model Loaded");
+}
